f84f92efba1ed46654ed3f9bb4ebb8ec08bf02cd
NatháliaKava <nathalia.kava@gmail.com>
2024-09-18 00:00:38 -0300

    Código completado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,24 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // DONE
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            // DONE
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// DONE
+
+Iphone meuIphone = new Iphone("(46) 99901-5911", "13 pro max", "4567", 256);
+meuIphone.Ligar();
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("Cifra Club");
+meuIphone.InstalarAplicativo("A Palheta Perdida");
+
+
+Nokia meuNokia = new Nokia("(46) 99901-5911", "Lumia", "2846", 64);
+meuNokia.Ligar();
+meuNokia.ReceberLigacao();
+meuNokia.InstalarAplicativo("Telegram");
+meuNokia.InstalarAplicativo("BitLife");
